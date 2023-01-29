6e8861f32c94f1822c496a43d673d4dcb35781cf
David Mayr <davidliebtkekse@gmail.com>
2023-01-29 02:09:14 +0100

    Some build changes

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,18 +1,44 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import io.papermc.paperweight.userdev.attribute.Obfuscation
+
 plugins {
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("maven-publish")
 }
 
 group = "eu.cafestube"
-version = "1.0.0-SNAPSHOT"
+version = "1.0.1-SNAPSHOT"
 
+val versionSpecific = configurations.create("versionSpecific") {
+    description = "Version Adapters to include in the JAR"
+    isCanBeConsumed = false
+    isCanBeResolved = true
+    shouldResolveConsistentlyWith(configurations["runtimeClasspath"])
+    attributes {
+        attribute(Obfuscation.OBFUSCATION_ATTRIBUTE, objects.named(Obfuscation.OBFUSCATED))
+    }
+}
+
+val versions = listOf("1_19_R1", "1_19_1_R1", "1_19_3_R2")
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
     implementation(project(":abstraction"))
-    implementation(project(":1_19_R1"))
-    implementation(project(":1_19_1_R1"))
+    versions.forEach {
+        versionSpecific(project(":$it"))
+    }
+}
 
+tasks.named<ShadowJar>("shadowJar") {
+    dependsOn(versions.map { project.project(":$it") }.map { it.tasks.named("build") })
+    from(Callable {
+        versionSpecific.resolve()
+            .map { f ->
+                zipTree(f).matching {
+                    exclude("META-INF/")
+                }
+            }
+    })
 }
 
 java {
